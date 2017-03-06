931478f751d13a82aed49ab1cfef6fec729a2f7b
jollaman999 <admin@jollaman999.com>
2017-03-07 01:46:04 +0900

    zram: Update to check crypto lz4

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -43,7 +43,7 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
-#ifdef CONFIG_ZRAM_LZ4_COMPRESS
+#ifdef CONFIG_CRYPTO_LZ4
 static const char *default_compressor = "lz4";
 #else
 static const char *default_compressor = "lzo";
